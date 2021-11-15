0483624add669c45c6846a3710853112dcaaf671
Kat <kyevsukov@gmail.com>
2021-11-15 18:05:47 -0500

    added last seed

--- api/data/seeds/07-classes_students.js
+++ api/data/seeds/07-classes_students.js
@@ -1,13 +1,11 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function(knex, Promise) {
+  return knex('classes_students').insert([   
+    { student_id: 3, class_id: 1},
+    { student_id: 4, class_id: 2},
+    { student_id: 2, class_id: 3},
+    { student_id: 1, class_id: 4},
+    { student_id: 4, class_id: 1},
+    { student_id: 3, class_id: 3},
+  ]);
 };
