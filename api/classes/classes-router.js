12b8895c247cfe3635b10a4f5f568d21dd629842
Kat <kyevsukov@gmail.com>
2021-11-16 11:06:01 -0500

    updated seeds again

--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -1,4 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Classes = require('./classes-model')
+const { restricted } = require("./../auth/auth-middleware")
+
+router.get('/', async (req, res, next) => {
+    try {
+        const classes = await Classes.findAll()
+        res.status(200).json(classes)
+    } catch(err){
+        next(err)
+    }
+})
 
 module.exports = router
